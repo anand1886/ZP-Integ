715521ebd9a47d9daa645354d42009003b3d07c5
anand1886 <anand.elumalai@zohocorp.com>
2013-09-11 20:14:34 +0530

    Update ShowDetails.java

--- ShowDetails.java
+++ ShowDetails.java
@@ -1,7 +1,9 @@
 import java.io.*;
 public Class ShowDetails{
   public static void main(String[] args){
+    String userName = "TEST";
     System.out.print("USER DETAILS:");
     //Code edited by sub user
+    System.out.print("NAME:"+userName);
   }
 }
